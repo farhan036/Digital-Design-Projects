53b7d18ad8f51797ddf68f707bd74a1f5823636d
farhan036 <mostafa.farhan4@gmail.com>
2025-10-14 16:49:21 +0300

    Keep low

--- Single Cycle RV-32I Processor/Adder.v
+++
@@ -1,7 +0,0 @@
-module Adder (
-    input wire [31:0] A,
-    input wire [31:0] B,
-    output wire [31:0] out
-);
-    assign out = A + B;
-endmodule
--- Single Cycle RV-32I Processor/run.do
+++
@@ -1,5 +0,0 @@
-vlib work
-vlog *.*v
-vsim -voptargs="+acc" work.RISC_tb
-do wave.do 
-run -all
